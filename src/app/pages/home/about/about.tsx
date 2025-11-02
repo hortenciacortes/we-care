801c281a694e81f834ce4644d5a46576e158b70e
Hortencia <hortencia.cortes@gmail.com>
2025-11-02 12:48:37 -0400

    UPDATE: modify About section content, update image imports, and adjust swiper width

--- src/app/pages/home/about/about.tsx
+++ src/app/pages/home/about/about.tsx
@@ -7,32 +7,28 @@ import style from "./about.module.scss";
 import { Autoplay, Navigation, Pagination } from 'swiper/modules';
 import ExportedImage from 'next-image-export-optimizer';
 
-import slide1 from '@assets/images/about/renata-swiper1.jpg';
-import slide2 from '@assets/images/about/renata-swiper2.jpg';
-import slide3 from '@assets/images/about/renata-swiper3.jpg';
+import slide1 from '@assets/images/about/slide1.jpg';
+import slide2 from '@assets/images/about/slide2.jpeg';
+import slide3 from '@assets/images/about/slide3.jpg';
+import slide4 from '@assets/images/about/slide4.jpg';
+import slide5 from '@assets/images/about/slide5.webp';
 
 export function About() {
   return (
     <section id='about' className="grid-pattern">
       <div className={style.about}>
         <div className={style.text}>
-          <h2>Sobre mim</h2>
+          <h2>QUEM SOMOS</h2>
           <p>
-            Sou médica formada pela Faculdade Metropolitana, movida pela vocação de
-            cuidar das pessoas com atenção, respeito e empatia.
+            Na WeCare Odontologia, acreditamos no poder transformador de um sorriso saudável. Há 10 anos, nos dedicamos a oferecer excelência em tratamentos odontológicos e a promover bem-estar, confiança e felicidade para cada paciente que atendemos.
           </p>
 
           <p>
-            Acredito que a medicina vai além do diagnóstico: é sobre ouvir,
-            compreender e oferecer o melhor cuidado possível para cada paciente.
+            Nossa missão é mais do que cuidar de sorrisos, é transformar vidas. Com uma equipe qualificada e apaixonada, combinamos conhecimento técnico com empatia para garantir um atendimento ético, humano e acolhedor.
           </p>
 
           <p>
-            Atendo como clínica geral, acompanhando casos de saúde preventiva,
-            orientação médica e tratamento de condições comuns, sempre buscando
-            criar uma relação de confiança e acolhimento. Meu compromisso é unir
-            conhecimento técnico com um olhar humano, ajudando meus pacientes a
-            viverem com mais saúde e qualidade de vida.
+            Valorizamos a individualidade de cada paciente. Nosso compromisso é entender suas necessidades e proporcionar tratamentos que aliem tecnologia de ponta, conforto e resultados duradouros.
           </p>
         </div>
 
@@ -72,6 +68,28 @@ export function About() {
               <div className={style.swiperImage}>
                 <ExportedImage
                   src={slide3}
+                  alt="Foto da médica"
+                  aria-hidden="true"
+                  sizes="(max-width: 768px) 100vw, 600px"
+                  fill
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={style.swiperImage}>
+                <ExportedImage
+                  src={slide4}
+                  alt="Foto da médica"
+                  aria-hidden="true"
+                  sizes="(max-width: 768px) 100vw, 600px"
+                  fill
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={style.swiperImage}>
+                <ExportedImage
+                  src={slide5}
                   alt="Foto da médica"
                   aria-hidden="true"
                   sizes="(max-width: 768px) 100vw, 600px"
